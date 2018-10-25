56e103601db1863a61033bc1fa860ed40461f5aa
Liam Egan <liam@wethecollective.com>
2018-10-25 16:28:04 -0700

    Adding the non-core event descriptions

--- src/js/application/Events.js
+++ src/js/application/Events.js
@@ -9,4 +9,50 @@ const pauseEvent = (e)=> {
   return false;
 }
 
+/**
+ * @module Application
+ */
+/**
+ * Casts a resize event cast from to the application.
+ *
+ * @event viewport-resize
+ * @type {object}
+ * @property {number} width - The width to resize the viewport to
+ * @property {number} height - The height to resize the viewport to
+ */
+/**
+ * Casts a set background event and sets the background with the value provided.
+ *
+ * @event set-background
+ * @type {object}
+ * @property {String|PIXI:Container} value - The background element to add.
+ */
+/**
+ * Casts a add sticker event and sets the background with the value provided.
+ * can operate with either a texture identifier or a sticker object.
+ *
+ * @event add-sticker
+ * @type {object}
+ * @property {String|PIXI:Container} value - The sticker element to add.
+ */
+/**
+ * Casts the drop sticker event and adds the specified sticker.
+ * NB: The position values should be normalised to the canvas PRIOR
+ * to being sent to this listener.
+ * This can be done using something like:
+ * ```
+    offset = $(@canvas).offset()
+    left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+    top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+
+    adjustedGlobalPosition = {x: position.x - offset.left + left, y: position.y - offset.top + top}
+   ```
+ *
+ *
+ * @event add-sticker
+ * @type {object}
+ * @property {DisplayObject:Sticker} value - The sticker element to add.
+ * @property {Object} position - the position of the dropped sticker on the canvas
+ */
+
 export { eventListener as default, pauseEvent };
